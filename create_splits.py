d5621d4b7e1f5290de0b528f889bf8e125bb5f3f
alice <bow1226@gmail.com>
2021-12-06 19:51:17 +0800

    feat: Implement create the splits

--- create_splits.py
+++ create_splits.py
@@ -2,9 +2,9 @@ import argparse
 import glob
 import os
 import random
-
+from pathlib import Path
 import numpy as np
-
+from sklearn.cross_validation import train_test_split
 from utils import get_module_logger
 
 
@@ -17,8 +17,21 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    tfrecords = glob.glob(os.path.join(source, "*.tfrecord"))
+    train_files, val_test_files = train_test_split(tfrecords, test_size=0.4)
+    val_files, test_files= train_test_split(val_test_files, test_size=0.5)
+    training = os.mkdir(os.path.join(source, 'train'), exist_ok=True)
+    val = os.mkdir(os.path.join(source, 'val'), exist_ok=True)
+    test = os.mkdir(os.path.join(source, 'test'), exist_ok=True)
 
+    dirs = [training, val, test]
+    files = [train_files, val_files, test_files]
+
+    for dir, file in zip(dirs, files):
+        print(f"Moving files to {dir}.")
+        for file in files:
+            Path(file).rename(os.path.join(dir, Path(file).name))
+        print(f"Loaded {len(files)} files.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
