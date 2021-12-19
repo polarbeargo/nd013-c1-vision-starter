c9bc516f2ca8e6fc3492401c274a0012cf3d14ea
alice <bow1226@gmail.com>
2021-12-19 09:04:24 +0800

    fix: FileExistsError:

--- create_splits.py
+++ create_splits.py
@@ -18,14 +18,14 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     tfrecords = glob.glob(os.path.join(source, "*.tfrecord"))
-    train_files, val_test_files = train_test_split(tfrecords, test_size=0.4)
-    val_files, test_files= train_test_split(val_test_files, test_size=0.5)
-    training = os.mkdir(os.path.join(source, 'train'), exist_ok=True)
-    val = os.mkdir(os.path.join(source, 'val'), exist_ok=True)
-    test = os.mkdir(os.path.join(source, 'test'), exist_ok=True)
+    train_files, val_files = train_test_split(tfrecords, test_size=0.4)
+#     val_files, test_files= train_test_split(val_test_files, test_size=0.5)
+    train = os.makedirs(os.path.join(destination, 'train'), exist_ok=True)
+    val = os.makedirs(os.path.join(destination, 'val'), exist_ok=True)
+#     test = os.mkdir(os.path.join(source, 'test'), exist_ok=True)
 
-    dirs = [training, val, test]
-    files = [train_files, val_files, test_files]
+    dirs = [train, val]
+    files = [train_files, val_files]
 
     for dir, file in zip(dirs, files):
         print(f"Moving files to {dir}.")
