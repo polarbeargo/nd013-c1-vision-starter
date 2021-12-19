6aaf602a5892d218f9b4e094b0e65e5f663f2e0b
alice <bow1226@gmail.com>
2021-12-19 11:03:39 +0800

    fix: expected str bytes or os pathlike object not nonetype

--- create_splits.py
+++ create_splits.py
@@ -4,6 +4,7 @@ import os
 import random
 from pathlib import Path
 import numpy as np
+import shutil
 from sklearn.model_selection import train_test_split
 from utils import get_module_logger
 
@@ -14,23 +15,24 @@ def split(source, destination):
     same directory. This folder should be named train, val and test.
 
     args:
-        - source [str]: source data directory, contains the processed tf records
+        - source [str]: source data directory, contains the processed tf records:         /home/workspace/data/waymo/training_and_validation/
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     tfrecords = glob.glob(os.path.join(source, "*.tfrecord"))
     train_files, val_test_files = train_test_split(tfrecords, test_size=0.4)
     val_files, test_files= train_test_split(val_test_files, test_size=0.5)
-    train = os.makedirs(os.path.join(destination, 'train'), exist_ok=True)
-    val = os.makedirs(os.path.join(destination, 'val'), exist_ok=True)
-    test = os.makedirs(os.path.join(source, 'test'), exist_ok=True)
+    
+    train = os.path.join(destination, 'train')
+    val = os.path.join(destination, 'val')
+    test = os.path.join(destination, 'test')
 
     dirs = [train, val, test]
     files = [train_files, val_files, test_files]
 
-    for dir, fs in zip(dirs, files):
-        print(f"Moving files to {dir}.")
+    for d, fs in zip(dirs, files):
+        print(f"Moving files to {d}.")
         for file in fs:
-            Path(file).rename(os.path.join(dir, Path(file).name))
+             shutil.move(file, d)
         print(f"Loaded {len(fs)} files.")
 
 if __name__ == "__main__":
